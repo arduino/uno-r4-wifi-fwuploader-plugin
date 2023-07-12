e5a49459f4e33e6ccd5814d48b4510ca36a8b399
Alessio Perugini <alessioper.98@gmail.com>
2023-07-12 16:06:40 +0200

    add license headers to *.go files

--- serial/utils/utils.go
+++ serial/utils/utils.go
@@ -1,3 +1,20 @@
+// This file is part of uno-r4-wifi-fwuploader-plugin.
+//
+// Copyright (c) 2023 Arduino LLC.  All right reserved.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 // Those function are token from https://github.com/arduino/arduino-cli/blob/master/arduino/serialutils/serialutils.go
 // that's because we don't have the `tr` here and importing the serialutils from the cli will lead to a panic
 package utils
